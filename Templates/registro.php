093c6ee190fcaeb7ed7d3106076f68e56b21471b
marioguddry <mario99cocolego@gmail.com>
2016-05-24 15:18:17 -0500

    Cifrado

--- Templates/registro.php
+++ Templates/registro.php
@@ -1,12 +1,16 @@
 <?php
 	function cifrar($p)
 	{
+		// Selecciona la sal y pimienta
+		
 		for($i=0;$i<5;$i++)
 		{
 			$x1 = rand(36,62);
 			$c1 = chr($x1+$x1);
-			$x2 = rand(16,62);
+			
+			$x2 = rand(32,62);
 			$c2 = chr($x2+$x2);
+			
 			if(!isset($ascii1))
 				$ascii1 = $c1;
 			else
@@ -17,43 +21,47 @@
 			else
 				$ascii2 = $ascii2 . $c2;
 		}
-		echo "<br/>".$ascii1."<br/>";
-		echo $ascii2."<br/>";
+		
+		//.
+		
+		// Cifra el código de la contraseña
 		
 		$c = strlen($p);
 		for($i=0;$i<$c;$i++)
 		{
 			$concat = substr($p, $i, 1);
 			$ascii = ord($concat);
-			$ascii = $ascii+10;
+			$ascii = $ascii+5;
+			if($ascii > 122)
+				$ascii = $ascii-85;
 			$resul = chr($ascii);
 			if(!isset($r))
 				$r = $resul;
 			else
 				$r = $r . $resul;
 		}
-		echo '<br/>'.$r.'<br/>';
-		$r = t2($r);
-		$r = $ascii1 . $r;
-		$r = $r . $ascii2;
-		echo $r.'<br/>';
-		return;
+		$rn = t2($r);
+		$rn = $ascii1 . $rn;
+		$rn = $rn . $ascii2;
+		
+		return $rn;
 	}
+	
+	//.
+	
+	// Se usa un hash y se pierde cierta información al sumar los valores en ascii de dos números
 	
 	function t2($res)
 	{
 		$a = strlen($res);
-		echo '<br/>'.$a.'<br/>';
 		if($a%2 == 0)
 		{
 			for($i=0;$i<$a;$i+=2)
 			{
 				$r = substr($res,$i,2);
-				//echo $r.'<br/>';
 				for($j=0;$j<=1;$j++)
 				{
 					$s = substr($r,$j,1);
-					echo "<br/>".$s;
 					if(!isset($t1))
 						$t1 = $s;
 					else
@@ -62,22 +70,44 @@
 				$d1 = ord($t1);
 				$d2 = ord($t2);
 				$d = $d1 + $d2;
-				if($d > 126)
-					$d = $d - 130;
-				elseif($d < 32)
-					$d = $d - 32;
-				echo "<br/>".$d;
+				if($d >	 126)
+					$d = $d - 134;
 				$e = chr($d);
-				echo "<br>".$e;
 				if(!isset($rn))
 					$rn = $e;
 				else
 					$rn = $rn . $e;
-				echo "<br/>".$rn."<br/>";
 			}
-			echo "<br/>".$rn."<br/>";
 		}
+		else
+		{
+			for($i=0;$i<$a+1;$i+=2)
+			{
+				$r = substr($res,$i,2);
+				for($j=0;$j<=1;$j++)
+				{
+					$s = substr($r,$j,1);
+					if(!isset($t1))
+						$t1 = $s;
+					else
+						$t2 = $s;
+				}
+				$d1 = ord($t1);
+				$d2 = ord($t2);
+				$d = $d1 + $d2;
+				if($d >	 126)
+					$d = $d - 134;
+				$e = chr($d);
+				if(!isset($rn))
+					$rn = $e;
+				else
+					$rn = $rn . $e;
+			}
+		}
+		return $rn;
 	}
+	
+	// termina el hash.
 	
 echo '<title> Seguridad </title>';
 		
@@ -86,7 +116,7 @@ echo '<title> Seguridad </title>';
 echo	'<form action="registro.php" method="POST">
 			Nombre de usuario: <input type="text" maxleght="15" name="nombre" required/><small> Tu nombre puede tener números y letras, pero no caracteres especiales, y puede ser de 5 a 15 caracteres </small><br/>
 			Contraseña: <input type="password" name="password" maxleght="15" required/>
-			<small> Puede contener letras, números, y algunos carácteres especiales como: ( _ / . / - / @ ). Debe de tener una longitud mínima de 8 caracters y puede llegar hasta 15 </small><br/>
+			<small> Puede contener letras o números. Debe de tener una longitud mínima de 8 caracters y puede llegar hasta 15 </small><br/>
 			<input type="submit" value="Registrarse">
 		</form>';
 		$value = rand(1,10);
@@ -121,8 +151,8 @@ echo	'<form action="registro.php" method="POST">
 				<a href='registro.php'> Volver a intentar </a>";
 			else
 			{
-				$nc = "/\w{5,15}/";
-				$pc = "/[a-zA-Z0-9\@\_\.\-]{8,15}/";
+				$nc = "/[a-zA-Z0-9\@]{5,15}/";
+				$pc = "/[a-zA-Z0-9]{8,15}/";
 				$compn = preg_match($nc, $nombre);
 				$compp = preg_match($pc, $pass);
 				if($compn != 0)
@@ -131,11 +161,10 @@ echo	'<form action="registro.php" method="POST">
 						<a href="registro.php"> Volver a intentar </a>';
 					else
 					{
-						//echo 'Bienvenid@: '.$nombre.
-						//'<a href="proyecto_seg.html"> Continuar </a>';
-						cifrar($pass);
-						//echo "contra";
-						//mysqli_query($conect, "INSERT INTO user VALUES ('$nombre','$contra')");
+						echo 'Bienvenid@: '.$nombre.
+						'<a href="proyecto_seg.html"> Continuar </a>';
+						$contra = cifrar($pass);
+						mysqli_query($conect, "INSERT INTO user VALUES ('$nombre','$contra')");
 					}
 				else
 					echo 'Tu cadena no es válida
